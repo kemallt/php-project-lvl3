d90f6b98d116d0c4479a00a10f9ab4cfa02ac014
kemallt <kemallt@gmail.com>
2021-11-11 15:25:48 +0300

    add navbar

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="/css/app.css" rel="stylesheet">
+    <title>{{ env('APP_NAME')  }}</title>
+</head>
+<body>
+    <header class="flex-shrink-0">
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+            <a class="navbar-brand" href="{{route('urls.create')}}">{{env('APP_NAME')}}</a>
+            <button
+                class="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation">
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a @class(["nav-link", 'active' => route('urls.create') === Request::url()]) href="{{route('urls.create')}}">Главная</a>
+                    </li>
+                    <li class="nav-item">
+                        <a @class(["nav-link", 'active' => route('urls.index') === Request::url()]) href="{{route('urls.index')}}">Сайты</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    <main class="flex-grow-1">
+        @yield('content')
+    </main>
+<script src="/js/app.js"></script>
+</body>
