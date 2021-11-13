03ef16e8d7a210256b1250e2c7c208ed35d8285b
kemallt <kemallt@gmail.com>
2021-11-13 19:40:37 +0300

    commit nullables in tables

---
+++ database/migrations/2021_11_13_133922_make_nullable_fields.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class MakeNullableFields extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('url_checks', function (Blueprint $table) {
+            $table->string('h1')->nullable()->change();
+            $table->string('title')->nullable()->change();
+            $table->string('description')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('url_checks', function (Blueprint $table) {
+            //
+        });
+    }
+}
--- tests/Feature/UrlTest.php
+++ tests/Feature/UrlTest.php
@@ -17,7 +17,7 @@ class UrlTest extends TestCase
 {
     use DatabaseMigrations;
 
-    protected $urls;
+    protected $urls, $url, $statusCode;
 
     protected function setUp(): void
     {
@@ -25,33 +25,36 @@ class UrlTest extends TestCase
             ['name' => 'https://google.com', 'created_at' => Carbon::now()],
             ['name' => 'https://yandex.ru', 'created_at' => Carbon::now()]
         ];
+        $this->url = $this->urls[0];
+        $this->url['id'] = 1;
+        $this->statusCode = 200;
         parent::setUp();
         DB::table('urls')->insert($this->urls);
     }
 
-    public function testCreate()
+    public function testCreate(): void
     {
         $response = $this->get(route('urls.create'));
-        $response->assertStatus(200);
+        $response->assertStatus($this->statusCode);
         $response->assertSee('Проверить');
     }
 
-    public function testIndex()
+    public function testIndex(): void
     {
         $response = $this->get(route('urls.index'));
-        $response->assertStatus(200);
+        $response->assertStatus($this->statusCode);
         $response->assertSee($this->urls[0]['name']);
         $response->assertSee($this->urls[1]['name']);
     }
 
-    public function testShow()
+    public function testShow(): void
     {
-        $response = $this->get(route('urls.show', ['url' => 1]));
-        $response->assertSee($this->urls[0]['name']);
+        $response = $this->get(route('urls.show', ['url' => $this->url['id']]));
+        $response->assertSee($this->url['name']);
         $response->assertSee('Имя');
     }
 
-    public function testStore()
+    public function testStore(): void
     {
         $url = ['name' => 'https://mail.ru'];
         $response = $this->post(route('urls.store', ['url' => $url]));
@@ -60,7 +63,7 @@ class UrlTest extends TestCase
         $this->assertDatabaseHas('urls', $url);
     }
 
-    public function testStoreError()
+    public function testStoreError(): void
     {
         $url = ['name' => ''];
         $response = $this->post(route('urls.store', ['url' => $url]));
@@ -68,39 +71,68 @@ class UrlTest extends TestCase
         $response->assertRedirect();
     }
 
-    public function testStoreDouble()
+    public function testStoreDouble(): void
     {
-        $url = $this->urls[0];
-        $response = $this->post(route('urls.store', ['url' => $url]));
+        $response = $this->post(route('urls.store', ['url' => $this->url]));
         $this->assertEquals('Страница уже существует', session('status'));
-        $response->assertRedirect(route('urls.show', ['url' => 1]));
+        $response->assertRedirect(route('urls.show', ['url' => $this->url['id']]));
     }
 
-    public function testCheck()
+    public function testCheck(): void
     {
-        $url = $this->urls[0];
-        $url['id'] = 1;
-        $statusCode = 200;
-        $html = file_get_contents(__DIR__ . '/../fixtures/test.html');
         Http::fake([
-            $url['name'] => Http::response($html, 200)
+            $this->url['name'] => Http::response('html body', $this->statusCode)
         ]);
-        $mockHtmlReturn = function ($retText) {
-            $parsingStub = new class($retText) {
-                private $retText;
-                public function __construct($retText)
-                {
-                    $this->retText = $retText;
-                }
-                public function attr() {
-                    return $this->retText;
-                }
-                public function text() {
-                    return $this->retText;
-                }
-            };
-            return $parsingStub;
-        };
+        $mockHtmlReturn = $this->getMockHtmlReturn();
+        $this->getDocumentMock($mockHtmlReturn);
+        $response = $this->post(route('urls.checks', ['url' => $this->url['id']]));
+        $this->assertDatabaseHas('url_checks', [
+            'url_id' => $this->url['id'],
+            'status_code' => $this->statusCode
+        ]);
+        $response->assertRedirect(route('urls.show', ['url' => $this->url['id']]));
+        $this->followRedirects($response)->assertStatus($this->statusCode)->assertSee($this->statusCode)->assertSee('h1 mock text');
+    }
+
+    public function testCheckWithoutH1(): void
+    {
+        Http::fake([
+            $this->url['name'] => Http::response('html body', $this->statusCode)
+        ]);
+        $this->getDocumentMockWithNulls();
+        $response = $this->post(route('urls.checks', ['url' => $this->url['id']]));
+        $this->assertDatabaseHas('url_checks', [
+            'url_id' => $this->url['id'],
+            'status_code' => $this->statusCode,
+            'h1' => null,
+            'title' => null,
+            'description' => null
+        ]);
+        $this->followRedirects($response)->assertStatus($this->statusCode)->assertSee($this->statusCode);
+    }
+
+    private function getDocumentMockWithNulls(): void
+    {
+        $documentMock = Mockery::mock('overload:\DiDom\Document');
+        $documentMock
+            ->shouldReceive('first')
+            ->with('h1')
+            ->once()
+            ->andReturn(null);
+        $documentMock
+            ->shouldReceive('first')
+            ->with('title')
+            ->once()
+            ->andReturn(null);
+        $documentMock
+            ->shouldReceive('first')
+            ->with('meta[name=description]')
+            ->once()
+            ->andReturn(null);
+    }
+
+    private function getDocumentMock($mockHtmlReturn): void
+    {
         $documentMock = Mockery::mock('overload:\DiDom\Document');
         $documentMock
             ->shouldReceive('first')
@@ -117,9 +149,26 @@ class UrlTest extends TestCase
             ->with('meta[name=description]')
             ->once()
             ->andReturnUsing(fn () => $mockHtmlReturn('description mock text'));
-        $response = $this->post(route('urls.checks', ['url' => $url['id']]));
-        $this->assertDatabaseHas('url_checks', ['url_id' => $url['id'], 'status_code' => $statusCode]);
-        $response->assertRedirect(route('urls.show', ['url' => $url['id']]));
-        $this->followRedirects($response)->assertStatus(200)->assertSee($statusCode);
+    }
+
+    private function getMockHtmlReturn(): callable
+    {
+        return function ($retText) {
+            return new class ($retText) {
+                private $retText;
+                public function __construct($retText)
+                {
+                    $this->retText = $retText;
+                }
+                public function attr()
+                {
+                    return $this->retText;
+                }
+                public function text()
+                {
+                    return $this->retText;
+                }
+            };
+        };
     }
 }
